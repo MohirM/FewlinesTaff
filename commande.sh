7b948068a0bd93b60838d7f1fafcdaf377b6810e
DavidAmegavi <amegavi.dav@gmail.com>
2021-03-10 19:15:22 +0100

    Fix my issue lol

--- commande.sh
+++ commande.sh
@@ -6,3 +6,4 @@ touch boubou_manual.txt
 echo "ouvre moi petit boubou c'est bien !" > boubou_manual.txt
 echo "BOUBOU" >> boubou_manual.txt 
 
+sorry bug quand j'ai crée la nouvelle branche j'ai modif main
