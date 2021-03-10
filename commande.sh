fc0634818e67b10a54d22a8c5f6c9f7dddb241c9
DavidAmegavi <amegavi.dav@gmail.com>
2021-03-10 19:08:36 +0100

    Fix your issue

--- commande.sh
+++ commande.sh
@@ -1,1 +1,8 @@
- ls, echo, cat
+#!/bin/bash
+mkdir boubou
+cd boubou 
+ls 
+touch boubou_manual.txt 
+echo "ouvre moi petit boubou c'est bien !" > boubou_manual.txt
+echo "BOUBOU" >> boubou_manual.txt 
+
